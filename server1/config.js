1ec4bd5c5a34b5a129c3ea41ecc3e6dbf7600cfc
Aimable <niyoai@yahoo.fr>
2018-02-23 10:40:48 +0000

    Add files via upload

---
+++ server1/config.js
@@ -0,0 +1,5 @@
+//Hold application secrets and config
+
+module.exports = {
+    secret: 'fkldfghfldkjghldksjgk3458276bsnfv'
+};
---
+++ server1/models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
+
+//Define models
+const userSchema =  new Schema({
+    email: { type: String, unique: true, lowercase: true },// mongo must know that the email will be unique and in lowercase.
+    password: String
+});
+
+//On save Hook, encrypt password
+// Before saving a model, run this function
+userSchema.pre('save', function(next){
+    const user = this;// get access to user model
+
+    //Generate a salt then run callback
+    bcrypt.genSalt(10, function(err, salt){
+        if(err){
+            return next(err);
+        }
+
+        // hash (encrypt) our password using the salt
+        bcrypt.hash(user.password, salt, null, function (err, hash) {
+            if(err) {
+                return next(err);
+            }
+
+            //Overwrite plain text password with encrypted password
+            user.password = hash;
+            next();// Go head and save model
+        });
+    });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, callback){
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+        if(err){
+            return callback(err);
+        }
+        callback(null, isMatch);
+    })
+};
+
+//Create the model class
+const ModelClass = mongoose.model('user', userSchema);
+
+//Export the model
+module.exports = ModelClass;
